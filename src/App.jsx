aa896ec691cb91b6d76b55005e0ae1d325562cdc
sam <sammybonfanti@gmail.com>
2022-04-03 20:17:18 -0700

    added the particles and rank components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,63 @@
-import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-tsparticles'
 
 import './App.css'
 
 function App() {
+	const particleOptions = {
+		interactivity: {
+			events: {
+				onHover: {
+					enable: true,
+					mode: 'repulse'
+				},
+				resize: true
+			}
+		},
+		particles: {
+			color: {
+				value: '#ffffff'
+			},
+			links: {
+				color: '#ffffff',
+				distance: 150,
+				enable: true,
+				opacity: 0.5,
+				width: 1
+			},
+			move: {
+				direction: 'none',
+				enable: true,
+				outMode: 'bounce',
+				random: false,
+				speed: 6,
+				straight: false
+			},
+			number: {
+				density: {
+					enable: true,
+					area: 800
+				},
+				value: 30
+			},
+			opacity: {
+				value: 0.5
+			}
+		}
+	}
+
 	return (
 		<>
 			<Navigation />
 			<Logo />
+			<Rank />
+			<ImageLinkForm />
 			{/*
-            <ImageLinkForm />
             <FaceRecognition />*/}
+			<Particles className='particles' options={particleOptions} />
 		</>
 	)
 }
