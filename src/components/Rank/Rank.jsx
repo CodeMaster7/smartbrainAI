d51fb5827f894bafab51c43497b9fbc74273939a
sam <sammybonfanti@gmail.com>
2022-04-03 20:18:42 -0700

    Rank component

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+		<>
+			<div className='white f3 tc'>{'Andrei, your current rank is...'}</div>
+			<div className='white f1 tc'>{'#5'}</div>
+		</>
+	)
+}
+export default Rank
