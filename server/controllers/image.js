247d632eff9c30ca58ea41a3c474262858aa4a7a
sam <sammybonfanti@gmail.com>
2022-04-08 01:24:06 -0700

    added the clarifai api call

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,14 +1,29 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+	apiKey: '49ce455d64a14e77b528a255cafb7e0a'
+})
+
+
 module.exports = {
-	handleImage: (db) => (req, res) => {
-		const { id } = req.body
+    handleImage: (db) => (req, res) => {
+        const { id } = req.body
 
 		db('users')
-			.where('id', '=', id)
-			.increment('entries', 1)
-			.returning('entries')
-			.then((entries) => {
-				res.json(entries[0].entries)
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then((entries) => {
+            res.json(entries[0].entries)
+        })
+        .catch((err) => res.status(400).json('unable to get count'))
+	},
+    handleApiCall:(req, res) => {
+        app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+			.then((data) => {
+				res.json(data)
 			})
-			.catch((err) => res.status(400).json('unable to get count'))
-	}
+			.catch((err) => res.status(400).json('unable to work with API'))
+    }
 }
