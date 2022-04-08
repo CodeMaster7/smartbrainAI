24803f6e805a7838bbe2ade6c25c6c70cf626ec6
sam <sammybonfanti@gmail.com>
2022-04-08 00:28:30 -0700

    added the register controller

---
+++ server/controllers/register.js
@@ -0,0 +1,33 @@
+module.exports = {
+	handleRegister: (db, bcrypt) => (req, res) => {
+		const { name, email, password } = req.body
+		//Get the hash
+		const hash = bcrypt.hashSync(password)
+
+		//Start transaction
+		db.transaction((trx) => {
+			//Insert into login table
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+				.into('login')
+				.returning('email')
+				.then((loginEmail) => {
+					//Insert into users table
+					return trx('users')
+						.returning('*')
+						.insert({
+							email: loginEmail[0].email,
+							name: name,
+							joined: new Date()
+						})
+						.then((user) => {
+							res.json(user[0])
+						})
+				})
+				.then(trx.commit)
+				.catch(trx.rollback)
+		}).catch((err) => res.status(400).json('unable to register...'))
+	}
+}
