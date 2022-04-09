6a0f30325cf0eb2c667005f2fb24dfb72a2a6960
sam <sammybonfanti@gmail.com>
2022-04-08 22:03:44 -0700

    new changes

--- index.js
+++ index.js
@@ -24,9 +24,18 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
+if (process.env.NODE_ENV === production) {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // ====== ENDPOINTS ====== //
 // /--> res = returns 'this is working'
-app.get('/', (req, res) => res.send('success!!'))
+// app.get('/', (req, res) => res.send('success!!'))
 
 // /signin --> POST = returns success/fail
 // dependency ingection
@@ -40,12 +49,6 @@ app.put('/image', image.handleImage(db))
 app.post('/imageurl', image.handleApiCall)
 
 
-// Set static folder
-app.use(express.static('client/build'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 
 app.listen(process.env.PORT || 5000, () => console.log(`listening on port: ${process.env.PORT}`))
